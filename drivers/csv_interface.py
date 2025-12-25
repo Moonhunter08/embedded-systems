608db84d1b8a0ebc4e0c8986344231693c506ddc
Julius Gummersbach <gummersbach@acronio.de>
2025-12-25 18:35:46 +0800

    Revert "Refactor CSV_Interface and CrashDetector to use threading for improved concurrency"
    This reverts commit b9b841ad88d3e32d09d0fb82c4ed5b242e24c582.

--- drivers/csv_interface.py
+++ drivers/csv_interface.py
@@ -1,7 +1,7 @@
+
+
 class CSV_Interface:
     def __init__(self, filename):
-        import _thread
-        self.lock = _thread.allocate_lock()
         self.filename = filename
         self.impact_count = 0
         
@@ -15,14 +15,12 @@ class CSV_Interface:
         
 
     def write_row(self, row):
-        with self.lock:
-            self.file = open(self.filename, 'a')
-            line = ','.join(str(item) for item in row) + '\n'
-            self.file.write(line)
-            self.file.flush()
-            self.file.close()
+        self.file = open(self.filename, 'a')
+        line = ','.join(str(item) for item in row) + '\n'
+        self.file.write(line)
+        self.file.flush()
+        self.file.close()
 
     def get_content(self):
-        with self.lock:
-            with open(self.filename, 'r') as file:
-                return file.read()
+        with open(self.filename, 'r') as file:
+            return file.read()
